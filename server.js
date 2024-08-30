34b5c053df3af4d7063f515255abe4f9dccaab25
RJ Cunanan <rjlugtuc@gmail.com>
2024-08-30 14:43:40 -0700

    Worked on hashing methods for secure password entries.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,7 +11,6 @@ const database = {
             id: '123',
             name: 'John',
             email: 'john@gmail.com',
-            password: 'cookies',
             entries: 0,
             joined: new Date()
         },
@@ -18,9 +18,15 @@ const database = {
             id: '124',
             name: 'Sally',
             email: 'sally@gmail.com',
-            password: 'bananas',
             entries: 0,
             joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
         }
     ]
 }
@@ -30,6 +36,13 @@ app.get('/', (req, res) => {
 } )
 
 app.post('/signin', (req, res) => {
+    // Load hash from your password DB.
+    bcrypt.compare("apples", '$2a$10$eigyRm69dm6U6joSkxssD.Sx7b3SUTbC/fow0hfF5cKuBOkkRCqU6', function(err, res) {
+        console.log('first guess', res)
+    });
+    bcrypt.compare("veggies", '$2a$10$eigyRm69dm6U6joSkxssD.Sx7b3SUTbC/fow0hfF5cKuBOkkRCqU6', function(err, res) {
+        console.log('second guess', res)
+    });
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
         res.json('success');
@@ -40,6 +53,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    // bcrypt.hash(password, null, null, function(err, hash) {
+    //     console.log(hash);
+    // });
     database.users.push({
         id: '125',
         name: name,
@@ -79,6 +95,12 @@ app.put('/image', (req, res) => {
         res.status(400).json('not found');
     }
 })
+
+
+
+
+
+
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
