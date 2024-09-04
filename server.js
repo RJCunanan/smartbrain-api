c9c0d33dca177337e787d174abaf6e037bb9e5dc
RJ Cunanan <rjlugtuc@gmail.com>
2024-09-04 11:55:25 -0700

    Started connecting frontend to backend.

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
         {
             id: '123',
             name: 'John',
+            password: 'cookies',
             email: 'john@gmail.com',
             entries: 0,
             joined: new Date()
@@ -17,6 +20,7 @@ const database = {
         {
             id: '124',
             name: 'Sally',
+            password: 'bananas',
             email: 'sally@gmail.com',
             entries: 0,
             joined: new Date()
@@ -45,7 +49,7 @@ app.post('/signin', (req, res) => {
     });
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-        res.json('success');
+        res.json(database.users[0]);    // return a real user from our "mock" database
     } else {
         res.status(400).json('error logging in');
     }
@@ -60,7 +64,6 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     })
